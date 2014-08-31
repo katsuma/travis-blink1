c556168927ff5f29ed5cf42ddb7bc1599ba0978a
ryo katsuma <katsuma@gmail.com>
2014-08-31 22:06:47 +0900

    Add spec setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,21 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
 require 'travis/blink1'
+require 'simplecov'
+require 'coveralls'
+
+include Travis::Blink1
+
+RSpec.configure do |config|
+  config.color = true
+  config.tty = true
+end
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start do
+ add_filter '/spec/'
+end
